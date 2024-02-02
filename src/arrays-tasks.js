ba23f7f849e412ace4ecf0d669a28e3c0eddb6b2
R P <romanunicum@gmail.com>
2024-02-02 12:47:06 +0100

    feat: do all

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -481,8 +481,9 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  const res = arr.sort((a, b) => b - a);
+  return res.slice(0, n);
 }
 
 /**
@@ -497,8 +498,8 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((elem) => arr2.includes(elem));
 }
 
 /**
@@ -512,8 +513,38 @@ function findCommonElements(/* arr1, arr2 */) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  const parts = nums
+    .reduce((acc, val, index, arr) => {
+      acc.push(arr.slice(0, index + 1));
+      return acc;
+    }, [])
+    .slice(1);
+  const allParst = [];
+  parts.map((elem) => {
+    return elem
+      .reduce((acc, val, index, arr) => {
+        acc.push(arr.slice(-index));
+        return acc;
+      }, allParst)
+      .slice(2);
+  });
+
+  const res = allParst
+    .filter((elem) => {
+      if (elem.length === 1) return false;
+
+      return elem.every((value, index, arr) => {
+        if (index === 0) return true;
+        if (value >= arr[index - 1]) return true;
+        return false;
+      });
+    })
+    .reduce((acc, value) => {
+      if (value.length > acc) return value.length;
+      return acc;
+    }, 0);
+  return res;
 }
 
 /**
@@ -530,8 +561,10 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, val, index) => {
+    return acc.concat(new Array(index + 1).fill(val));
+  }, []);
 }
 
 /**
@@ -547,8 +580,10 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0
+    ? arr.slice(-n).concat(arr.slice(0, n + 1))
+    : arr.slice(-n).concat(arr.slice(0, -n));
 }
 
 /**
@@ -564,8 +599,22 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const mapa = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  return arr.sort((a, b) => {
+    return mapa[a] - mapa[b];
+  });
 }
 
 /**
@@ -587,8 +636,17 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length <= 1) return arr;
+  const n = Math.floor(arr.length / 2);
+  console.log(arr.length);
+
+  return arr.length % 2 === 0
+    ? arr.slice(-n).concat(arr.slice(0, n))
+    : arr
+        .slice(-n)
+        .concat(arr.slice(n, n + 1))
+        .concat(arr.slice(0, -n - 1));
 }
 
 module.exports = {
